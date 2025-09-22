61b95f894c83d2cbdd8ed5bc8f41d791b2bd16a8
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2025-09-22 15:09:25 +0530

    root set to new repository

--- routes/web.php
+++ routes/web.php
@@ -6,31 +6,33 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::redirect('/', 'dashboard');
-
-Route::group(['middleware' => ['auth:sanctum']], function () {
-
-        // DASHBOARD
-        Route::get('dashboard', function () {
-            return Inertia::render('Dashboard');
-        })->name('dashboard');
-
-        // PROFILE
-        Route::get('/profile', array( ProfileController::class, 'edit' ))->name('profile.edit');
-        Route::patch('/profile', array( ProfileController::class, 'update' ))->name('profile.update');
-        Route::delete('/profile', array( ProfileController::class, 'destroy' ))->name('profile.destroy');
-        Route::post('profile/avatar', [ProfileController::class, 'updateAvatar'])
-            ->name('profile.avatar.update');
-        // USERS
-
-        Route::get('/users', [UserController::class, 'index'])->name('users.index');
-        Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-        Route::post('/users', [UserController::class, 'store'])->name('users.store');
-        Route::get('/users/{id}', [UserController::class, 'findById'])->name('users.show');
-        Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-        Route::patch('/users/{id}', [UserController::class, 'update'])->name('users.update');
-        // Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-        Route::delete('/users/bulk-destroy', [UserController::class, 'bulkDestroy'])->name('users.bulk-destroy');
+Route::get('/', function () {
+    return Inertia::render('Home');
 });
+
+// Route::group(['middleware' => ['auth:sanctum']], function () {
+
+//         // DASHBOARD
+//         Route::get('dashboard', function () {
+//             return Inertia::render('Dashboard');
+//         })->name('dashboard');
+
+//         // PROFILE
+//         Route::get('/profile', array( ProfileController::class, 'edit' ))->name('profile.edit');
+//         Route::patch('/profile', array( ProfileController::class, 'update' ))->name('profile.update');
+//         Route::delete('/profile', array( ProfileController::class, 'destroy' ))->name('profile.destroy');
+//         Route::post('profile/avatar', [ProfileController::class, 'updateAvatar'])
+//             ->name('profile.avatar.update');
+//         // USERS
+
+//         Route::get('/users', [UserController::class, 'index'])->name('users.index');
+//         Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+//         Route::post('/users', [UserController::class, 'store'])->name('users.store');
+//         Route::get('/users/{id}', [UserController::class, 'findById'])->name('users.show');
+//         Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+//         Route::patch('/users/{id}', [UserController::class, 'update'])->name('users.update');
+//         // Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+//         Route::delete('/users/bulk-destroy', [UserController::class, 'bulkDestroy'])->name('users.bulk-destroy');
+// });
 
 require __DIR__ . '/auth.php';
