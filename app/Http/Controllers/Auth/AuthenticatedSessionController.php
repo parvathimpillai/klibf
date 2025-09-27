97d1a38fff2b1c76925abf3cd33dea0dece4ef7a
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2025-09-27 17:22:07 +0530

    User registration-login-using email password-done

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -27,21 +27,21 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
+    public function store(LoginRequest $request)
     {
         $request->authenticate();
 
         $request->session()->regenerate();
 
         // return redirect()->intended(route('filament.admin.dashboard', absolute: false));
-        return redirect()->intended(route('filament.admin.pages.dashboard'));
-        // return Inertia::location('filament.admin.pages.dashboard');
+        // return redirect()->to(route('filament.admin.pages.dashboard'));
+        return Inertia::location(route('filament.admin.pages.dashboard'));
     }
 
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): RedirectResponse
+    public function destroy(Request $request)
     {
         Auth::guard('web')->logout();
 
@@ -49,7 +49,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        return redirect()->to(route('login'));
         // return Inertia::location('/login');
     }
 }
